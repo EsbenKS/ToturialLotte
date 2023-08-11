7846f9b9ad800edc4d867ff0e92af933d4168d04
EsbenKS <33330790+EsbenKS@users.noreply.github.com>
2023-08-11 07:33:11 +0200

    Add files via upload

--- ArrayListExESBEN/src/Main.java
+++ ArrayListExESBEN/src/Main.java
@@ -7,72 +7,81 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Scanner scnFile = new Scanner(new File("c:\\Datamatiker\\Toturial\\ArrayListEx\\src\\names.txt"));
-        ArrayList<String>  list = new ArrayList<>();
+        Scanner scnFile = new Scanner(new File("names.txt"));
+        ArrayList<String> list = new ArrayList<>();
 
-        while(scnFile.hasNext()){
+        while (scnFile.hasNext()) {
             list.add(scnFile.next());
         }
+
         scnFile.close();
 
-        System.out.println(list);
-
-        Scanner scn = new Scanner(System.in);
-
-        System.out.println("1: See list of names");
-        System.out.println("2: Add a name to list");
-        System.out.println("3: Edit a name from the list");
-        System.out.println("4: Remove a name from the list");
+        int input;
+        do {
+            Scanner scn = new Scanner(System.in);
 
 
-        int input = scn.nextInt();
-        switch (input){
-            case 1:
-                System.out.println(list);
-                break;
+            System.out.println("0: Exit");
+            System.out.println("1: See list of names");
+            System.out.println("2: Add a name to list");
+            System.out.println("3: Edit a name from the list");
+            System.out.println("4: Remove a name from the list");
 
-            case 2:
-                System.out.print("Enter name to add to the list: ");
 
-                Scanner scnIn = new Scanner(System.in);
-                list.add(scnIn.next());
-                System.out.println(list);
+            input = scn.nextInt();
+            switch (input) {
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
 
-                addToList(list);
+                case 1:
+                    System.out.println(list);
+                    break;
 
-                break;
+                case 2:
+                    System.out.print("Enter name to add to the list: ");
 
-            case 3:
-                System.out.println(list);
+                    Scanner scnIn = new Scanner(System.in);
+                    list.add(scnIn.next());
+                    System.out.println(list);
 
-                System.out.print("Enter name from list to replace: ");
-                Scanner scnTarget = new Scanner(System.in);
-                String target = scnTarget.next();
-                System.out.print("Enter new name added to name list: ");
-                Scanner scnRepl = new Scanner(System.in);
-                String repl = scnRepl.next();
+                    addToList(list);
 
-                replace(list, target, repl);
-                addToList(list);
-                System.out.println(list);
-                break;
+                    break;
 
-            case 4:
-                System.out.println(list);
-                System.out.print("Enter name to delete from the list: ");
-                Scanner scnDelete = new Scanner(System.in);
-                String delete = scnDelete.next();
+                case 3:
+                    System.out.println(list);
 
-                delete(list, delete);
-                addToList(list);
-                System.out.println(list);
+                    System.out.print("Enter name from list to replace: ");
+                    Scanner scnTarget = new Scanner(System.in);
+                    String target = scnTarget.next();
+                    System.out.print("Enter new name added to name list: ");
+                    Scanner scnRepl = new Scanner(System.in);
+                    String repl = scnRepl.next();
 
-            default:
-                System.out.println("Please enter a number between 1-??");
-        }
+                    replace(list, target, repl);
+                    addToList(list);
+                    System.out.println(list);
+                    break;
+
+                case 4:
+                    System.out.println(list);
+                    System.out.print("Enter name to delete from the list: ");
+                    Scanner scnDelete = new Scanner(System.in);
+                    String delete = scnDelete.next();
+
+                    delete(list, delete);
+                    addToList(list);
+                    System.out.println(list);
+
+
+                default:
+                    System.out.println("Please enter a number between 1-??");
+            }
+        } while (input != 0);
     }
     public static void addToList(ArrayList<String> tempList)throws IOException{
-        FileWriter writer = new FileWriter("c:\\Datamatiker\\Toturial\\ArrayListEx\\src\\names.txt");
+        FileWriter writer = new FileWriter("names.txt");
         for(String s: tempList){
             writer.write(s + System.lineSeparator());
         }
